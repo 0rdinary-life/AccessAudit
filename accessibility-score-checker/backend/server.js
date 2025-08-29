3ac8db273bc268285832d0dac981620677afec1d
The-red-ant <anthonysinchi655@gmail.com>
2025-08-29 09:18:11 -0400

    test

--- accessibility-score-checker/backend/server.js
+++ accessibility-score-checker/backend/server.js
@@ -9,7 +9,7 @@ app.use('.api/scan', scanRoutes)
 app.get('/api/health', (req, res) => {
     res.json({status: "ok"})
 })
-
+console.log("hello from server.js")
 const port = process.env.port || 3000;
 app.listen(port, ()=> {
     console.log(`liisting to port ${port}`)
