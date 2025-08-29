57931ec15ae26df91dcae2145f5c91738ae22089
The-red-ant <anthonysinchi655@gmail.com>
2025-08-28 22:34:34 -0400

    did the back end

--- accessibility-score-checker/backend/server.js
+++ accessibility-score-checker/backend/server.js
@@ -9,7 +9,6 @@ app.use('.api/scan', scanRoutes)
 app.get('/api/health', (req, res) => {
     res.json({status: "ok"})
 })
-console.log(process.env.port)
 
 const port = process.env.port || 3000;
 app.listen(port, ()=> {
